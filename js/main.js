ba019d4de80a188736a21c9245b9b8a46a547bd5
Omina-Omonullaeva <aminaamanullaeva@gmail.com>
2023-09-23 14:22:56 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let userPrice = prompt("Yonizda necha so'm pul bor:")
 
 let elTitle = document.querySelector('.title');
 
-if (userPrice >= 12,585,600) {
+if (userPrice >= 12585600) {
     elTitle.textContent = `Oq yo'l, ${userName}`
 }else{
     elTitle.textContent = `${userName}, ozgina sabr qilish kerak ekan`
